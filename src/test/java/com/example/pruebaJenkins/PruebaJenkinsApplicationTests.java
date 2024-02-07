3f40fca950b1650c94391b449f26424cd7ea8134
Albertoho09 <albertoraikeor@gmail.com>
2024-02-07 14:17:21 +0100

    first commit

---
+++ src/test/java/com/example/pruebaJenkins/PruebaJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebaJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
